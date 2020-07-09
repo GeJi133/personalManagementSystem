f53757d1d8ce9554cdd9e6d14194edba3497c7b5
wsh1376015660 <1376015660@qq.com>
2020-07-09 16:32:21 +0800

    现在有 登录 修改密码 提出离职、部门调动申请 这些后端功能接口（王世豪）

--- src/main/java/org/csu/personalManagementSystem/controller/AccountController.java
+++ src/main/java/org/csu/personalManagementSystem/controller/AccountController.java
@@ -23,7 +23,7 @@ public class AccountController {
     @ResponseBody
     public AppResult<String> Login(@RequestParam("username") String username, @RequestParam("password") String  password) {
         AppResult<String> appResult = new AppResult<>();
-        Account account = accountService.getAccount (username,DigestUtils.md5DigestAsHex(password.getBytes()));
+        Account account = accountService.getAccountByUsernameAndPassword (username,password);
         System.out.println ("接受到请求");
         System.out.println (account);
         if(account == null)
@@ -38,88 +38,99 @@ public class AccountController {
         return appResult;
     }
 
-    @UserLoginToken
-    @GetMapping(value = "/username/{username}",produces="application/Json;charset=UTF-8")
-    public AppResult<Account> getAccountByUsername(@PathVariable("username")String username){
-        AppResult<Account> appResult = new AppResult<>();
-        Account account = accountService.getAccountByUsername (username);
-        if(account == null)
-        {
-            appResult = ResultBuilder.fail(ResultCode.UsernameNotFound);
-        }
-        else
-        {
-            appResult = ResultBuilder.successData(ResultCode.OK,account);
-        }
-        return appResult;
-    }
 
-    @PassToken
-    @GetMapping(value = "/phone/{phone}", produces="application/Json;charset=UTF-8" )
-    @ResponseBody
-    public AppResult<Account> getAccountByPhone(@PathVariable("phone")String phone) {
-        AppResult<Account> appResult = new AppResult<>();
-        Account account = accountService.getAccountByPhoneNumber (phone);
-        if (account == null)
-        {
-            appResult = ResultBuilder.fail(ResultCode.PhoneNotFound);
-        }
-        else
-        {
-            appResult = ResultBuilder.successData(ResultCode.OK,account);
-        }
-        return appResult;
-    }
-
-    @UserLoginToken
-    @PutMapping(value = "/",produces="application/Json;charset=UTF-8")
-    public AppResult<String> updateAccount(@RequestBody Account account){
-        AppResult<String> appResult = new AppResult<>();
-        if(account.getPassword ()!=null){
-      account.setPassword (DigestUtils.md5DigestAsHex(account.getPassword().getBytes()));}
-        accountService.updateAccount (account);
-        appResult = ResultBuilder.successNoData(ResultCode.Handled);
-        return appResult;
-    }
-
-    @UserLoginToken
     @PatchMapping(value = "/",produces="application/Json;charset=UTF-8")
     public AppResult<String> updatePassword(@RequestBody Account account){
         AppResult<String> appResult = new AppResult<>();
-        account.setPassword (DigestUtils.md5DigestAsHex(account.getPassword().getBytes()));
         accountService.updatePassword (account);
         appResult = ResultBuilder.successNoData(ResultCode.Handled);
         return appResult;
     }
 
-    @PassToken
-    @PostMapping(value = "/", produces="application/Json;charset=UTF-8" )
-    @ResponseBody
-    public AppResult<String> insertAccount(@RequestBody Account account ){
-        AppResult<String> appResult = new AppResult<>();
-      System.out.println (account.getPassword());
-        account.setPassword (DigestUtils.md5DigestAsHex(account.getPassword ().getBytes()));
-        System.out.println (account.getPassword());
-        accountService.insertAccount (account);
-        appResult = ResultBuilder.successNoData(ResultCode.Handled);
-        return appResult;
-    }
 
-    @PassToken
-    @GetMapping( "/sendVCode" )
-    public AppResult<String> sendVCode(@RequestParam("phone") String phone) throws ClientException {
-        AppResult<String> appResult = new AppResult<>();
-        System.out.println ("手机号"+phone);
-        String sms = accountService.sendMsg (phone);
-        String msg;
-        System.out.println ("sms:"+sms);
-        if(sms != null){
-            appResult = ResultBuilder.successNoData(ResultCode.Handled);
-        }
-        else {
-            appResult = ResultBuilder.fail(ResultCode.VerifyCodeNotSend);
-        }
-        return appResult;
-    }
+
+//    @UserLoginToken
+//    @GetMapping(value = "/username/{username}",produces="application/Json;charset=UTF-8")
+//    public AppResult<Account> getAccountByUsername(@PathVariable("username")String username){
+//        AppResult<Account> appResult = new AppResult<>();
+//        Account account = accountService.getAccountByUsernameAndPassword(username,pass);
+//        if(account == null)
+//        {
+//            appResult = ResultBuilder.fail(ResultCode.UsernameNotFound);
+//        }
+//        else
+//        {
+//            appResult = ResultBuilder.successData(ResultCode.OK,account);
+//        }
+//        return appResult;
+//    }
+//
+//    @PassToken
+//    @GetMapping(value = "/phone/{phone}", produces="application/Json;charset=UTF-8" )
+//    @ResponseBody
+//    public AppResult<Account> getAccountByPhone(@PathVariable("phone")String phone) {
+//        AppResult<Account> appResult = new AppResult<>();
+//        Account account = accountService.getAccountByPhoneNumber (phone);
+//        if (account == null)
+//        {
+//            appResult = ResultBuilder.fail(ResultCode.PhoneNotFound);
+//        }
+//        else
+//        {
+//            appResult = ResultBuilder.successData(ResultCode.OK,account);
+//        }
+//        return appResult;
+//    }
+//
+//    @UserLoginToken
+//    @PutMapping(value = "/",produces="application/Json;charset=UTF-8")
+//    public AppResult<String> updateAccount(@RequestBody Account account){
+//        AppResult<String> appResult = new AppResult<>();
+//        if(account.getPassword ()!=null){
+//      account.setPassword (DigestUtils.md5DigestAsHex(account.getPassword().getBytes()));}
+//        accountService.updateAccount (account);
+//        appResult = ResultBuilder.successNoData(ResultCode.Handled);
+//        return appResult;
+//    }
+//
+//    @UserLoginToken
+//    @PatchMapping(value = "/",produces="application/Json;charset=UTF-8")
+//    public AppResult<String> updatePassword(@RequestBody Account account){
+//        AppResult<String> appResult = new AppResult<>();
+//        account.setPassword (DigestUtils.md5DigestAsHex(account.getPassword().getBytes()));
+//        accountService.updatePassword (account);
+//        appResult = ResultBuilder.successNoData(ResultCode.Handled);
+//        return appResult;
+//    }
+//
+//    @PassToken
+//    @PostMapping(value = "/", produces="application/Json;charset=UTF-8" )
+//    @ResponseBody
+//    public AppResult<String> insertAccount(@RequestBody Account account ){
+//        AppResult<String> appResult = new AppResult<>();
+//      System.out.println (account.getPassword());
+//        account.setPassword (DigestUtils.md5DigestAsHex(account.getPassword ().getBytes()));
+//        System.out.println (account.getPassword());
+//        accountService.insertAccount (account);
+//        appResult = ResultBuilder.successNoData(ResultCode.Handled);
+//        return appResult;
+//    }
+//
+//    @PassToken
+//    @GetMapping( "/sendVCode" )
+//    public AppResult<String> sendVCode(@RequestParam("phone") String phone) throws ClientException {
+//        AppResult<String> appResult = new AppResult<>();
+//        System.out.println ("手机号"+phone);
+//        String sms = accountService.sendMsg (phone);
+//        String msg;
+//        System.out.println ("sms:"+sms);
+//        if(sms != null){
+//            appResult = ResultBuilder.successNoData(ResultCode.Handled);
+//        }
+//        else {
+//            appResult = ResultBuilder.fail(ResultCode.VerifyCodeNotSend);
+//        }
+//        return appResult;
+//    }
 
 }
