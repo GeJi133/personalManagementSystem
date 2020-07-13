3a2bc6eb7b53f35b92f8dab99cd40dcdea6ec9ad
GeJi133 <1332535951@qq.com>
2020-07-13 18:04:11 +0800

    修改handle部分的自动合并

--- src/main/java/org/csu/personalManagementSystem/service/HandleService.java
+++ src/main/java/org/csu/personalManagementSystem/service/HandleService.java
@@ -1,14 +1,12 @@
 package org.csu.personalManagementSystem.service;
 
-<<<<<<< HEAD
 import org.csu.personalManagementSystem.domain.DepartmentTransfer;
 import org.csu.personalManagementSystem.domain.JobTransfer;
 import org.csu.personalManagementSystem.domain.Leaving;
 import org.csu.personalManagementSystem.domain.PositionTransfer;
-=======
+
 import org.csu.personalManagementSystem.domain.Leaving;
-import org.csu.personalManagementSystem.domain.Transfer;
->>>>>>> ba94bb41b27c1c0049548d36772fe8177d87911f
+
 import org.csu.personalManagementSystem.persistence.HandleMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -21,7 +19,6 @@ public class HandleService {
 
     public void handleLeaving(Leaving leaving){
 
-<<<<<<< HEAD
         handleMapper.handleLeaving1(leaving);
         handleMapper.handleLeaving2(leaving);
     }
@@ -39,16 +36,7 @@ public class HandleService {
     public void handlePositionTransfer(PositionTransfer positionTransfer){
         handleMapper.handlePositionTransfer1(positionTransfer);
         handleMapper.handlePositionTransfer2(positionTransfer);
-=======
-        handleMapper.handleLeaving(leaving);
+
     }
 
-    public void handleTransfer(Transfer transfer){
-        handleMapper.handleTransfer(transfer);
-    }
-
-    public void handleJobTransfer(String id,String wantedJob){
-        handleMapper.handleJobTransfer(id, wantedJob);
->>>>>>> ba94bb41b27c1c0049548d36772fe8177d87911f
-    }
 }
